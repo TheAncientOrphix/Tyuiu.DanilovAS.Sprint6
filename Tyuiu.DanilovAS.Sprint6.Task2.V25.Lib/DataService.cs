579914206fdf61fb0bb427a22da28802813e014c
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-11-14 08:47:34 +0500

    Task2

--- Tyuiu.DanilovAS.Sprint6.Task2.V25.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint6.Task2.V25.Lib/DataService.cs
@@ -13,15 +13,15 @@ namespace Tyuiu.DanilovAS.Sprint6.Task2.V25.Lib
 
             for(int i = startValue; i <= stopValue; i++)
             {
-                if ((Math.Sin(i) - 2) != 0)
-                {
+                //if ((Math.Sin(i) - 2) != 0)
+               // {
                     y = (5 * i + 2.5) / (Math.Sin(i) - 2) + 2;
                     y = Math.Round(y, 2);
-                }
-                else
-                {
-                    y = 0;
-                }
+                //}
+                //else
+               // {
+                //    y = 0;
+               // }
                 valueArray[count] = y;
                 count++;
             }
